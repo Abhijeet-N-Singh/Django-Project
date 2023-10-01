bfb37fa406f97ed0ce2df15183e16a3f6635fd55
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-10-01 13:05:57 +0530

    Add files via upload

--- Django_project/flipkart/shop/views.py
+++ Django_project/flipkart/shop/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
-from . models import Product
+from . models import Product,Contact
 from math import ceil
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
-# def index(request):
-#     products= Product.objects.all()
-#     n= len(products)
-#     nSlides= n//4 + ceil((n/4) + (n//4))
-#     params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
-#     return render(request,"shop/index.html", params)
 def index(request):
     products= Product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4)-(n//4))
-    allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
+    allProds=[]
+    catprods= Product.objects.values('category', 'id')
+    cats= {item["category"] for item in catprods}
+    
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    
     params={'allProds':allProds }
     return render(request,"shop/index.html", params)
+
+
 
 
     
@@ -26,18 +29,33 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    if request.method=="POST":
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+        return HttpResponseRedirect('/')
+
+
+
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
-def productView(request):
-    return HttpResponse("We are at product view")
+def productView(request, myid):
+    product=Product.objects.filter(id=myid)
+    print(product)
+    return render(request, "shop/prodView.html", {'product':product[0]})
+
+
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')
 
 
