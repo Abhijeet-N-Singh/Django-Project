088fb677b088e9f21f1a54e0dc9652dd64e3f31f
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-10-08 13:36:48 +0530

    Add files via upload

--- Django_project/flipkart/shop/admin.py
+++ Django_project/flipkart/shop/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product,Contact
+from .models import Product,Contact,Orders
 admin.site.register(Product)
 
 
 admin.site.register(Contact)
+
+admin.site.register(Orders)
+
 
